75f64590846c69fdb006a0abb844dad386c20118
Venkata Sai <144120718+venkat3153@users.noreply.github.com>
2026-06-21 18:56:51 +0530

    feat(decision): add app client mount

---
+++ indian-options-vibe/frontend/components/AppClientMount.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { MainTableFinalStatusDecorator } from '@/components/MainTableFinalStatusDecorator';
+
+export function AppClientMount() {
+  return <MainTableFinalStatusDecorator />;
+}
